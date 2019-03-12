96532c99aa7bcf22f99260d1f8125012eabbc489
Davijms15 <41484476+Davijms15@users.noreply.github.com>
2019-03-12 16:20:58 -0300

    Update MRU.py

--- MRU.py
+++ MRU.py
@@ -13,36 +13,40 @@ print("Insira a posição final: ")
 posicao_final = float(input())
 print("Insira a velocidade: ")
 velocidade = float(input())
-
-if(posicao_final < posicao_inicial):#Caso específico da velocidade ter sido digitada positiva quando deveria ser negativa
-     velocidade = -abs(velocidade)
-
+x = posicao_inicial
+y = 0
+lista = []
 
 fig = plt.figure('MRU')
 
 ax = fig.add_subplot(111) # o numero 111 define o tamanho na interface
 ax.grid(True, color = '0.75') # define a cor das linhas dos eixos das coordenadas
-ax.set_xlim([-abs(posicao_inicial) - 10, abs(posicao_final) + 10]) # amplia um intervalo dado no x
-ax.set_ylim([0, ((posicao_final-posicao_inicial) / velocidade) + 10]) # amplia um intervalo dado no x
+ax.set_xlim([-max(abs(posicao_inicial), abs(posicao_final)) * 15 / 10, abs(posicao_final) * 15 / 10]) # amplia um intervalo dado no x
+ax.set_ylabel('Time')
+ax.set_xlabel('Space')
 
 scat = plt.scatter(posicao_inicial, 0)
 scat.set_alpha(0.5) # seta a cor da bola
 
+if(velocidade != 0):
+     # Caso específico da velocidade ter sido digitada positiva quando deveria ser negativa
+     if(posicao_final < posicao_inicial):
+          velocidade = -abs(velocidade)
+     if(posicao_final == -posicao_inicial): # quando for 500 e -500
+          aux = abs(int((2 * posicao_final / velocidade))) * 10
+          print(aux)
+     else: # quando for -500 e 500
+          aux = abs(int((posicao_final - posicao_inicial) / velocidade)) * 10
+     for i in range(aux):  # *10 pra rodar o loop mais vezes e diminuir o tamanho so salto da bola
+          x += velocidade / 10
+          y += 0.1
+          lista.append([x, y])
+     ax.set_ylim([-1, ((posicao_final - posicao_inicial) / velocidade) + 10]) # amplia um intervalo dado no x
+else :
+     ax.set_ylim([-1, 10]) # amplia um intervalo dado no y
+
+
 # frames: distancia que ele vai percorrer
 # interval: velocidade
-lista = []
-x = posicao_inicial
-y = 0
-if(posicao_final == -posicao_inicial):
-     aux = 2 * abs(posicao_final) * 10
-else:
-    aux = abs(int((posicao_final - posicao_inicial) / velocidade)) * 10
-print(aux)
-for i in range(aux):# *10 pra rodar o loop mais vezes e diminuir o tamanho so salto da bola
-     x += velocidade / 10
-     y += 0.1
-     lista.append([x, y])
-     
 anim = animation.FuncAnimation(fig, _update_plot, fargs = (fig, scat), frames = lista, interval = 60)
-
 plt.show()
